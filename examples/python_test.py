637b69aa6741fb7b34914aebe1c3a2c74a4e3666
MC9529 <mc9529@163.com>
2019-12-08 00:17:16 +0800

    add the least_square

--- examples/python_test.py
+++ examples/python_test.py
@@ -4,6 +4,13 @@ import matplotlib.pyplot as plt
 
 e = np.array([1, 2, 3])
 f = np.array([2, 3, 4])
+a = np.append(e[1:3], f,axis = 0)
+m = np.array([[1,2,3], [2,2,3], [2,3,4]])
+print("the inverse:", type(np.linalg.inv(m)))
+g = np.vdot(e, f.transpose())
+print("the g: ", g)
+
+print("the a: ", a)
 
 for i in range(len(e) -1, 0, -1):
               e[i] = e[i - 1]
